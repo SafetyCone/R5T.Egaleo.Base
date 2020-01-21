3f5506570e96e2e1fece3054bdb084f6b1d0271a
MinexAutomation <minex.automation@gmail.com>
2020-01-21 12:18:15 -0800

    Added the IOrganizationDirectoryNameConvention service definition interface type.

---
+++ source/R5T.Egaleo.Base/Code/Services/Definitions/IOrganizationDirectoryNameConvention.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Egaleo
+{
+    public interface IOrganizationDirectoryNameConvention
+    {
+        string GetOrganizationDirectoryName(string organizationName);
+    }
+}
